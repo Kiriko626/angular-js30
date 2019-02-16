312f8b47a1e4cb63bfe6cfd87755736bdadba4b5
Kiriko <kiriko626@gmail.com>
2019-02-16 13:33:57 +0500

    Драм-машина

---
+++ src/app/drum-machine/drum-machine.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
+import { drumItems, IDrumItem } from "./drum-machine.const";
+
+@Component({
+    selector: 'drum-machine',
+    templateUrl:'./drum-machine.component.html',
+    styleUrls: ['./drum-machine.component.styl']
+})
+export class DrumMachineComponent {
+
+    public allKeys: IDrumItem[] = drumItems;
+
+    @HostListener('document:keypress', ['$event'])
+    private onKeyDown(event: KeyboardEvent) {
+        const audio = new Audio();
+        const activeItem = this.allKeys.find(item => item.key === event.keyCode);
+        if (activeItem) {
+            audio.src = './assets/sounds/' + activeItem.audio;
+            audio.load();
+            activeItem.isPlaying = true;
+            audio.play();
+        }
+    }
+}
---
+++ src/app/drum-machine/drum-machine.const.ts
@@ -0,0 +1,74 @@
+export interface IDrumItem {
+    key: number;
+    isPlaying: boolean;
+    title: string;
+    text: string;
+    audio: string;
+}
+
+export const drumItems: IDrumItem[] = [
+    {
+        key: 97,
+        audio:'clap.wav',
+        isPlaying: false,
+        text:'clap',
+        title: 'A'
+    },
+    {
+        key: 115,
+        audio:'hihat.wav',
+        isPlaying: false,
+        text:'hihat',
+        title: 'S'
+    },
+    {
+        key: 100,
+        audio:'kick.wav',
+        isPlaying: false,
+        text:'kick',
+        title: 'D'
+    },
+    {
+        key: 102,
+        audio:'openhat.wav',
+        isPlaying: false,
+        text:'openhat',
+        title: 'F'
+    },
+    {
+        key: 103,
+        audio:'boom.wav',
+        isPlaying: false,
+        text:'boom',
+        title: 'G'
+    },
+    {
+        key: 104,
+        audio:'ride.wav',
+        isPlaying: false,
+        text:'ride',
+        title: 'H'
+    },
+    {
+        key: 106,
+        audio:'snare.wav',
+        isPlaying: false,
+        text:'snare',
+        title: 'J'
+    },
+    {
+        key: 107,
+        audio:'tom.wav',
+        isPlaying: false,
+        text:'tom',
+        title: 'K'
+    },
+    {
+        key: 108,
+        audio:'tink.wav',
+        isPlaying: false,
+        text:'tink',
+        title: 'L'
+    },
+
+];
