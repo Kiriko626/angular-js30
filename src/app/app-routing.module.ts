90dca9ef98c46ab27e19cb4d0f42ff42eb18faec
Kiriko <kiriko626@gmail.com>
2019-02-16 13:05:19 +0500

    Наброски для главной страницы сайта

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DrumMachineComponent} from "./drum-machine/drum-machine.component";
+import {HomeComponent} from "./home/home.component";
+import {CanvasComponent} from "./canvas/canvas.component";
+import {ClockComponent} from "./clock/clock.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'drum',
+    component: DrumMachineComponent
+  },
+  {
+    path: 'canvas',
+    component: CanvasComponent
+  },
+  {
+    path: 'clock',
+    component: ClockComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
